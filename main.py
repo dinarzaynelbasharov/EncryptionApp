60efe1a62cb8219a26797770120a50f9578410cd
dinar <120639828+dinarzaynelbasharov@users.noreply.github.com>
2023-12-07 20:45:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import ctypes
 import os
 import threading
 
-deal = ctypes.CDLL("deal.dll", winmode = 0)
-luc = ctypes.CDLL("luc.dll", winmode = 0)
+deal = ctypes.CDLL("deal.dll", winmode=0)
+luc = ctypes.CDLL("luc.dll", winmode=0)
 
 deal.GenerateKeys(256)
 
 def reset_fields():
     file_entry.delete(0, tk.END)
+    if (len(key_entry.get()) != 0):
+        key_entry.delete(0, tk.END)
 
 def process_file():
     progress = tk.DoubleVar()
@@ -31,7 +33,10 @@ def process_file():
     if (selected_mode == "Шифрование"):
         mode = 1
     if (selected_mode == "Дешифрование"):
+        selected_key = key_entry.get()
+        key = selected_key.encode('cp866')
         mode = 0
+
     file = selected_file_path.encode('cp866')
     directorytosave = output_directory.encode('cp866')
     progress.set(30)
@@ -39,7 +44,10 @@ def process_file():
     if (selected_method == "DEAL"):
         deal.Encryption(file, directorytosave, 256, mode, cipher_mode)
     else:
-        luc.Encryption(file, directorytosave, mode)
+        if (mode == 1):
+            luc.LucEncryption(file, directorytosave, 256, mode)
+        else:
+            luc.LucDecryption(file, key, directorytosave, 256, mode)
     progress.set(80)
 
     if not selected_file_path or not output_directory:
@@ -61,13 +69,6 @@ def start_processing():
     t = threading.Thread(target=process_file)
     t.start()
 
-def on_select1(event):
-    value = method_choice.get()
-    if value == "DEAL":
-        cipher_mode_choice.state(["!disabled"])
-    else:
-        cipher_mode_choice.state(["disabled"])
-
 def check_file(*args):
     file_name = file_var.get()
     file2_name = file2_var.get()
@@ -75,6 +76,15 @@ def check_file(*args):
         next_button.config(state="normal")
     else:
         next_button.config(state="disabled")
+
+def on_selectmode(event):
+    value = mode_choice.get()
+    if value == "Шифрование":
+        browse_key_button.config(state="disabled")
+        key_entry.config(state="readonly")
+    else:
+        browse_key_button.config(state="normal")
+        key_entry.config(state="normal")
 
 app = tk.Tk()
 app.resizable(False, False)
@@ -87,8 +97,8 @@ frame1.pack()
 delete_file_var = tk.BooleanVar()
 delete_file_var.set(False)
 
-delete_file_checkbutton = ttk.Checkbutton(frame1, text="Удалить подаваемый файл", variable=delete_file_var)
-delete_file_checkbutton.grid(row=5, column=0, columnspan=3, pady=10)
+delete_file_checkbutton = ttk.Checkbutton(frame1, text="Удалить исходный файл", variable=delete_file_var)
+delete_file_checkbutton.grid(row=6, column=0, columnspan=3, pady=10)
 
 
 file_label = ttk.Label(frame1, text="Выберите файл:")
@@ -122,23 +132,31 @@ cipher_mode_choice = ttk.Combobox(frame1, state="readonly", values=["ECB", "CBC"
 cipher_mode_choice.current(0)
 cipher_mode_choice.grid(row=2, column=1)
 
-method_choice.bind("<<ComboboxSelected>>", on_select1)
+key_label = ttk.Label(frame1, text="Выберите ключ:")
+key_label.grid(row=4, column=0, pady=10)
+
+key_var = tk.StringVar()
+key_entry = ttk.Entry(frame1, state="readonly", width=50, textvariable=key_var)
+key_entry.grid(row=4, column=1)
+
+browse_key_button = ttk.Button(frame1, state="disabled", text="Обзор", command=lambda: key_var.set(filedialog.askopenfilename()))
+browse_key_button.grid(row=4, column=2, padx=10)
 
 output_label = ttk.Label(frame1, text="Выберите директорию для сохранения файла:")
-output_label.grid(row=4, column=0, pady=10, padx=10)
+output_label.grid(row=5, column=0, pady=10, padx=10)
 
 file2_var = tk.StringVar()
 output_entry = ttk.Entry(frame1, width=50, textvariable=file2_var)
 output_entry.insert(0, "C:\\Users\\t8met\\Desktop")
-output_entry.grid(row=4, column=1)
+output_entry.grid(row=5, column=1)
 
 browse_output_button = ttk.Button(frame1, text="Обзор", command=lambda: file2_var.set(filedialog.askdirectory(title="Выберите директорию для сохранения файла")))
-browse_output_button.grid(row=4, column=2, padx=10)
+browse_output_button.grid(row=5, column=2, padx=10)
 
-
+mode_choice.bind("<<ComboboxSelected>>", on_selectmode)
 
 next_button = ttk.Button(frame1, state="disabled", text="Ок", command=start_processing)
-next_button.grid(row=6, columnspan=3, pady=20)
+next_button.grid(row=7, columnspan=3, pady=20)
 
 file_var.trace("w", check_file)
 file2_var.trace("w", check_file)
